cf7a70fdca01a067cc8ecd22c8ded6f1c71deea6
MihTC <minhtran.kgka@gmail.com>
2024-06-17 06:10:59 +0700

    get list user and show on homepage

--- lib/services/database_service.dart
+++ lib/services/database_service.dart
@@ -1,12 +1,18 @@
 import 'package:chatapp/models/user_profile.dart';
+import 'package:chatapp/services/auth_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get_it/get_it.dart';
 
 class DatabaseService {
   final FirebaseFirestore _firebaseFirestore = FirebaseFirestore.instance;
+  final GetIt _getIt = GetIt.instance;
+
+  late AuthService _authService;
 
   CollectionReference? _usersCollection;
 
   DatabaseService() {
+    _authService = _getIt.get<AuthService>();
     _setupCollectionReferences();
   }
 
@@ -21,5 +27,11 @@ class DatabaseService {
 
   Future<void> createUserProfile({required UserProfile userProfile}) async {
     await _usersCollection?.doc(userProfile.uid).set(userProfile);
+  }
+
+  Stream<QuerySnapshot<UserProfile>>? getUserProfiles() {
+    return _usersCollection
+        ?.where("uid", isNotEqualTo: _authService.user!.uid)
+        .snapshots() as Stream<QuerySnapshot<UserProfile>>;
   }
 }
