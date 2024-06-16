bd5c6a12e0925c982f92670c82c86c8befd8b35d
MihTC <minhtran.kgka@gmail.com>
2024-06-17 05:07:57 +0700

    add userProfile to db

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
 import 'package:chatapp/consts.dart';
+import 'package:chatapp/models/user_profile.dart';
+import 'package:chatapp/services/alert_service.dart';
 import 'package:chatapp/services/auth_service.dart';
+import 'package:chatapp/services/database_service.dart';
 import 'package:chatapp/services/media_service.dart';
 import 'package:chatapp/services/navigation_service.dart';
 import 'package:chatapp/services/storage_service.dart';
@@ -29,6 +32,8 @@ class _RegisterPageState extends State<RegisterPage> {
   late NavigationService _navigationService;
   late AuthService _authService;
   late StorageService _storageService;
+  late AlertService _alertService;
+  late DatabaseService _databaseService;
 
   @override
   void initState() {
@@ -37,6 +42,8 @@ class _RegisterPageState extends State<RegisterPage> {
     _navigationService = _getIt.get<NavigationService>();
     _authService = _getIt.get<AuthService>();
     _storageService = _getIt.get<StorageService>();
+    _alertService = _getIt.get<AlertService>();
+    _databaseService = _getIt.get<DatabaseService>();
   }
 
   @override
@@ -171,15 +178,28 @@ class _RegisterPageState extends State<RegisterPage> {
               if (result) {
                 String? pfpURL = await _storageService.uploadUserPfp(
                     file: selectionImage!, uid: _authService.user!.uid);
-                //_navigationService.pushReplacementNamed("/home");
+                if (pfpURL != null) {
+                  await _databaseService.createUserProfile(
+                      userProfile: UserProfile(
+                          uid: _authService.user!.uid,
+                          name: name,
+                          pfpURL: pfpURL));
+                }
+                _alertService.showToast(
+                    text: "User registered successfully!", icon: Icons.check);
+                _navigationService.goBack();
+                _navigationService.pushReplacementNamed("/home");
               } else {
-                // _alertService.showToast(
-                //     text: "Failed to login, Please try again!!",
-                //     icon: Icons.error);
+                throw Exception("Unable to upload user profile picture");
               }
+            } else {
+              throw Exception("Unable to register user");
             }
           } catch (e) {
             print(e);
+            _alertService.showToast(
+                text: "Failed to register, Please try again!",
+                icon: Icons.error);
           }
           setState(() {
             isLoading = false;
